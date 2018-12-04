87da75623a80530e6a32e86ea5492d717ccbbd3a
achernyi <achernyi@luxoft.com>
2018-12-04 18:46:10 +0200

    Added the winner property

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,31 +3,34 @@ pragma solidity ^0.4.17;
 contract Lottery {
     address public manager;
     address [] public players;
+    address public winner;
     function Lottery() public {
         manager = msg.sender;
+        winner = 0;
     }
-    
+
     function enter() public payable {
         require(msg.value > .000001 ether);
         players.push(msg.sender);
     }
-    
+
     function random() private view returns (uint){
         return uint(sha256(block.difficulty, now, players));
     }
-    
-    function pickWinner() public managerRestricted playerRestricted payable {
-        if(0 != players.length){
-            uint index = random() % players.length;
-            players[index].transfer(this.balance);
 
-             players = new address[] (0);
-        }
+    function pickWinner() public managerRestricted playerRestricted payable {
+
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        winner = players[index];
+        players = new address[] (0);
+
     }
+
     function getPlayers() public view returns (address[] ){
         return players;
     }
-    
+
     modifier managerRestricted(){
         require(msg.sender == manager);
         _;
@@ -38,7 +41,4 @@ contract Lottery {
         _;
     }
 
-
-
-    
 }
